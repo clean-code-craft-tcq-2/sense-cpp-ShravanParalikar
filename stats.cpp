9e94bda4706e1c1cd3b7153cc4b004a20639283d
ShravanParalikar <91043470+ShravanParalikar@users.noreply.github.com>
2021-10-24 15:36:29 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,9 +1,20 @@
 #include "stats.h"
+#include <bits/stdc++.h>
 
 Stats Statistics::ComputeStatistics(const std::vector<float>&vect ) {
     //Implement statistics here
     Stats computedStats;
-
+   
+    if ( vect.empty() )
+    {
+      computedStats.min = NAN;
+      computedStats.max = NAN;
+      computedStats.average = NAN;
+        
+      return computedStats;
+    }
+    else
+    {
     int size = vect.size();
     float sum = 0;
 
@@ -12,6 +23,11 @@ Stats Statistics::ComputeStatistics(const std::vector<float>&vect ) {
         sum += vect[i];
     }
     computedStats.average = (float)sum/size;
+        
+    computedStats.min =  *min_element(vect.begin(),vect.end());
+    computedStats.max =  *max_element(vect.begin(),vect.end());
+        
     return computedStats;
+    }
     
 }
