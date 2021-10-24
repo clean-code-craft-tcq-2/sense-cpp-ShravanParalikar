491eab1c7ecb8b8d0ff0c6bcc441f0af4a486171
ShravanParalikar <91043470+ShravanParalikar@users.noreply.github.com>
2021-10-24 16:09:05 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -26,7 +26,7 @@ TEST_CASE("average is NaN for empty array") {
     REQUIRE(std::isnan(computedStats.min));
 }
 
-/*TEST_CASE("raises alerts when max is greater than threshold") {
+TEST_CASE("raises alerts when max is greater than threshold") {
     EmailAlert emailAlert;
     LEDAlert ledAlert;
     std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
@@ -37,4 +37,4 @@ TEST_CASE("average is NaN for empty array") {
 
     REQUIRE(emailAlert.emailSent);
     REQUIRE(ledAlert.ledGlows);
-} */
+}
