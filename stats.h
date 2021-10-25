f007180d77fb22468ea0a983bb4410e451d81f6c
ShravanParalikar <91043470+ShravanParalikar@users.noreply.github.com>
2021-10-25 10:44:28 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,17 +7,36 @@ struct Stats
     float min;
    };
 
-
-
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>&vect );
 }
-typedef void (*funcptr)();
-void checkAndAlert(float maxThreshold, funcptr IAlerter[], struct Stats computedStats);
 
-extern bool emailAlertCall;
-extern bool ledAlertCall;
-void emailAlerter();
-void ledAlerter();
+struct EmailAlert 
+{
+    bool emailSent;
+};
+struct LEDAlert 
+{
+    bool ledGlows;
+};
+struct IAlerter
+{
+    EmailAlert *emailAlertPtr;
+    LEDAlert *ledAlertPtr;
+};
 
+class StatsAlerter
+{
+    float maximumThreshold;
+    std::vector<IAlerter*>vecAlert;
+
+public:
+    StatsAlerter(const float Threshold,  std::vector<IAlerter*> vecAlertInit )
+    {
+        maximumThreshold =  Threshold;
+        vecAlert = vecAlertInit;
+    }
+    
+    void checkAndAlert( const std::vector<double>&);
+};
 
